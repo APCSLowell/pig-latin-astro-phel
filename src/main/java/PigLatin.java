339524f4adec677401d7972f54a15365287e7480
astro-phel <katgranads@gmail.com>
2025-02-05 08:31:22 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -36,19 +36,17 @@ public int findFirstVowel(String sWord){
 }
   return -1;
 }
-public String pigLatin(String sWord) {
-    // precondition: sWord is a valid String of length greater than 0
-    // postcondition: returns the pig latin equivalent of sWord
-    if (findFirstVowel(sWord) == -1) {
-      return sWord + "ay";
-    } 
-    if(findFirstVowel(sWord) == 0){
-      return sWord + "way";
-    } 
-    if(sWord.substring(0, 2).equals("qu")) {
-      return sWord.substring(2) + "qu" + "ay";
-    } 
-    else{
-      return sWord.substring(findFirstVowel(sWord))+sWord.substring(0, findFirstVowel(sWord)) + "ay";
-    }
+public String pigLatin(String sWord){
+//precondition: sWord is a valid String of length greater than 0
+//postcondition: returns the pig latin equivalent of sWord
+  if(findFirstVowel(sWord) == -1){
+    return sWord + "ay";
+  }
+  else if(findFirstVowel(sWord) == -2){
+    return sWord.substring(2, sWord.length()) + "quay";
+  }
+  else if(findFirstVowel(sWord) == 0){
+    return sWord + "way";
+  }
+  else return sWord.substring(findFirstVowel(sWord), sWord.length()) + sWord.substring(0, findFirstVowel(sWord)) + "ay";
 }
